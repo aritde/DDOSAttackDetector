59298c5c523c6b241dd8ce8c0fa23bf35f8187a0
aritde <aritde@indiana.edu>
2017-04-27 19:44:28 -0400

    Added comments and meaningful variables

--- ddosAttackDetector/src/ddosAttackDetector/ddosAttackDetection.java
+++ ddosAttackDetector/src/ddosAttackDetector/ddosAttackDetection.java
@@ -1,22 +1,26 @@
-package ddosAttackDetector;
+package phData;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
 public class ddosAttackDetection {
 	static private String inputFile = "E:/Transferred/Apps/GA/apache-access-log.txt";
-	static Map<String,HitStructure> map = new HashMap<>();
-	static Set<String> set = new HashSet<>();
+	static Map<String,HitStructure> mapOfRecords = new HashMap<>();
+	static Set<String> suspiciousIPs = new HashSet<>();
 	public static void main(String[] args)throws IOException
 	{
 		FileReader reader = new FileReader(new File(inputFile));
@@ -26,61 +30,48 @@ public class ddosAttackDetection {
     	Date minTimeData = null;
     	while((line = bufferedReader.readLine())!=null)
     	{
+    		//parses desired data from records
     		Record r = splitFields(line);
     		String ipAddress = r.getIpAddress();
     		System.out.println("Currently Processsing IP :" + ipAddress);
     		Date currentTime = r.getTimestamp();
-    		if(minTimeFlag == 0)
+    		//checks for occurence of IP in map
+			if(mapOfRecords.containsKey(ipAddress))
     		{
-    			minTimeFlag++;
-    			minTimeData = currentTime; 
-    		}
-    		else
-    		{
-	    		long diff = currentTime.getTime()-minTimeData.getTime();
-	    		long diffSeconds = diff /1000%60;
-	    		long diffMinutes = diff /(60*1000)%60;
-	    		long diffHours = diff /(60*60*1000);
-	    		if(diffHours>=0 && diffMinutes>=1 && diffSeconds>=1)
-				{
-	    			minTimeData = currentTime;
-					map.clear();
-				}
-    		}
-    		if(map.containsKey(ipAddress))
-    		{
-    			Date lastHitTime = map.get(ipAddress).getTimestamp();
-    			System.out.println("Last Hit Time " +lastHitTime );
-    			System.out.println("Curr Hit Time " +currentTime );
-    			long diff = currentTime.getTime()-lastHitTime.getTime();
+    			Date lastHitTime = mapOfRecords.get(ipAddress).getTimestamp();
+    			//Calculates time difference
+				long diff = currentTime.getTime()-lastHitTime.getTime();
     			long diffSeconds = diff /1000%60;
     			long diffMinutes = diff /(60*1000)%60;
     			long diffHours = diff /(60*60*1000);
     			if(diffHours==0 && diffMinutes<1)
     			{
-    				int updateCount = map.get(ipAddress).getCount();
+    				int updateCount = mapOfRecords.get(ipAddress).getCount();
     				HitStructure h = new HitStructure(lastHitTime,updateCount+1);
-    				map.put(ipAddress, h);
-    				if(updateCount+1 >=88)
+    				mapOfRecords.put(ipAddress, h);
+    				//Checks whether the current count exceeds the threshold or not
+					if(updateCount+1 >=88)
     				{
-    					if(!set.contains(ipAddress))
+    					// Adds the suspicious IP to the final result set
+						if(!suspiciousIPs.contains(ipAddress))
     					{
-	    					//System.out.println("Suspicious ID:" + ipAddress);
-	    					set.add(ipAddress);
+	    					suspiciousIPs.add(ipAddress);
 	    				}
     				}
     			}
     		}
     		else
     		{
-    			HitStructure h = new HitStructure(currentTime,1);
-    			map.put(ipAddress, h);
+    			//Resets the entry count to 1
+				HitStructure h = new HitStructure(currentTime,1);
+    			mapOfRecords.put(ipAddress, h);
     		}
     	}
-    	/*for(String ipa :set)
-			System.out.println("Suspicious ID:" + ipa);*/
-    	System.out.println("Suspicious ID:" + set.size());
+    	System.out.println("Suspicious ID:" + suspiciousIPs.size());
+    	//Writes the suspicious IP to the file.
+		printSuspiciousIPs(suspiciousIPs);
 	}
+	// Core function for parsing desired data from records
 	public static Record  splitFields(String line)
 	{
 		String[] fields = line.split(" ");
@@ -96,6 +87,21 @@ public class ddosAttackDetection {
 		System.out.println(r.getIpAddress()+" "+r.getTimestamp());
 		return r;
 	}
-
+	// Function for writing output to file
+	public static void printSuspiciousIPs(Set<String> suspiciousIPs) throws IOException {
+		try(FileWriter fw = new FileWriter("E:/Transferred/Apps/GA/suspiciousIP.txt", true);
+			    BufferedWriter bw = new BufferedWriter(fw);
+			    PrintWriter out = new PrintWriter(bw))
+			{
+			Iterator<String> ipIterator = suspiciousIPs.iterator();
+			while(ipIterator.hasNext())
+			{
+				out.println("IP Address :" + ipIterator.next());
+	        }
+			
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    }
 }
 
